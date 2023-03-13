a7c43424d1adfd1ba8b490587396526da8e4c8c2
Nandins-12 <fernandogdias12@gmail.com>
2023-03-13 02:45:23 -0300

    diminuição do javascript

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,10 @@ const buttons = document.querySelectorAll('.botao');
 const images = document.querySelectorAll('.imagem');
 
 buttons.forEach((button, indexButton) => {
-
     button.addEventListener('click', () => {
-
         buttons.forEach(button => {button.classList.remove('selecionado')});
         button.classList.add('selecionado');
-
-        images.forEach((image, indexImage) => {
-
-            image.classList.remove('ativa');
-
-            if(indexButton == indexImage) {
-                image.classList.add('ativa');
-            };
-
-        })
-
+        images.forEach(image => {image.classList.remove('ativa')});
+        images[indexButton].classList.add('ativa');
     })
-
 });
